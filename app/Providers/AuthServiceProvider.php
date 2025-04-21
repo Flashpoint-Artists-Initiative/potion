2e668fb11c1e184247d78497c57c280d3127be81
Syzgyn <git@syzgyn.com>
2025-04-20 20:48:50 -0400

    Pint

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,16 +6,9 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 
-use App\Enums\RolesEnum;
 use App\Models\User;
-use Carbon\Carbon;
-use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Lang;
-use Illuminate\Support\Facades\URL;
 
 class AuthServiceProvider extends ServiceProvider
 {
