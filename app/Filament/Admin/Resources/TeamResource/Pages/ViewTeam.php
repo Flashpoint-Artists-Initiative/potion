0b0b4f77dc3fd2747e8e2b7eb988cb6e4cec7566
Syzgyn <git@syzgyn.com>
2025-09-08 18:02:36 -0400

    Fix shift importer error when wrapping around midnight

--- app/Filament/Admin/Resources/TeamResource/Pages/ViewTeam.php
+++ app/Filament/Admin/Resources/TeamResource/Pages/ViewTeam.php
@@ -4,16 +4,10 @@ declare(strict_types=1);
 
 namespace App\Filament\Admin\Resources\TeamResource\Pages;
 
-use App\Enums\LockdownEnum;
 use App\Filament\Admin\Resources\TeamResource;
-use App\Filament\Imports\ShiftImporter;
-use App\Models\Event;
 use Filament\Actions;
-use Filament\Actions\ImportAction;
 use Filament\Resources\Pages\ViewRecord;
 use Guava\FilamentNestedResources\Concerns\NestedPage;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
 
 class ViewTeam extends ViewRecord
 {
@@ -23,21 +17,10 @@ class ViewTeam extends ViewRecord
 
     protected function getHeaderActions(): array
     {
-        $recordId = $this->record instanceof Model ? $this->record->getKey() : $this->record;
-        
         return [
             Actions\EditAction::make(),
             // Action::make('Edit Shifts')
             //     ->url(ManageShiftTypes::getUrl(['record' => $this->record])),
-            ImportAction::make()
-                ->label('Import Shifts')
-                ->importer(ShiftImporter::class)
-                ->options([
-                    'eventId' => Event::getCurrentEventId(),
-                    'teamId' => $recordId,
-                ])
-                ->chunkSize(30)
-                ->visible(fn () => Auth::user()?->can('teams.create') && ! LockdownEnum::Volunteers->isLocked()),
         ];
     }
 }
