f266e87a70c0c7bb32eeb0f83d3fcc333a7f7dee
Syzgyn <git@syzgyn.com>
2025-09-13 14:06:10 -0400

    Add GateScan model, migration, and relationships

---
+++ app/Models/Ticketing/GateScan.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models\Ticketing;
+
+use App\Models\Event;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use OwenIt\Auditing\Auditable;
+
+class GateScan extends Model
+{
+    use Auditable, HasFactory;
+
+    protected $fillable = [
+        'event_id',
+        'user_id',
+        'wristband_number',
+    ];
+
+    /**
+     * @return BelongsTo<Event, $this>
+     */
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    /**
+     * @return BelongsTo<User, $this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    
+}
