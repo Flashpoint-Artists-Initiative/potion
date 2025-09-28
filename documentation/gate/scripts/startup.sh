f1f860a983c23e9ad5a22d35dac2512edaf85dda
Will Johnson <willyum2n@gmail.com>
2025-09-28 13:39:34 -0400

    updated path

--- documentation/gate/scripts/startup.sh
+++ documentation/gate/scripts/startup.sh
@@ -1,4 +1,4 @@
 # This will start the local potion app on HTTP
 # HTTPS is hosted by nginx and will forward to port 8000
-cd /home/gate/potion/
+cd /home/gate/potion-app/
 php artisan serve --host=0.0.0.0 --port=8000
