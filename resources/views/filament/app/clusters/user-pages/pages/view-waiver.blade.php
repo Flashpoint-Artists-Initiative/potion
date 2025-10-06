147bcac2656c9770ce755fcb5ab60dbde35106c3
Syzgyn <git@syzgyn.com>
2025-10-06 12:57:04 -0400

    Add name and date when viewing signed waivers

--- resources/views/filament/app/clusters/user-pages/pages/view-waiver.blade.php
+++ resources/views/filament/app/clusters/user-pages/pages/view-waiver.blade.php
@@ -1,1 +1,9 @@
-{!! str($waiver->content)->sanitizeHtml() !!}
+<p>
+    {!! str($waiver->content)->sanitizeHtml() !!}
+</p>
+<p>
+    Signed: <span class="italic">{{ $completedWaiver->form_data['signature'] }}</span>
+</p>
+<p>
+    Date: {{ $completedWaiver->created_at->format('F j, Y, g:i a') }}
+</p>
