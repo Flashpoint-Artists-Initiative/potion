03eaf634a19bcd726b4b173b4046e49032563971
Syzgyn <git@syzgyn.com>
2025-10-10 19:27:28 -0400

    Fix user creation

--- app/Filament/Admin/Resources/UserResource.php
+++ app/Filament/Admin/Resources/UserResource.php
@@ -71,9 +71,10 @@ class UserResource extends Resource
                     ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at')
                     ->timezone('America/New_York'),
-                // Forms\Components\TextInput::make('password')
-                //     ->password()
-                //     ->maxLength(255),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->maxLength(255)
+                    ->hidden(fn ($operation) => $operation === 'edit'),
                 Forms\Components\Select::make('role_id')
                     ->multiple()
                     ->relationship('roles', 'name')
